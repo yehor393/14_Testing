1bab2c1d71495edf86094e70537de65ebb9364bd
yehor393 <yehor393@gmail.com>
2024-02-06 21:47:58 -0700

    Feb 6

--- app/test/test_unit_repository.py
+++ app/test/test_unit_repository.py
@@ -29,11 +29,47 @@ def test_activation_success(activation_data, mock_db):
             username="test@mail.com",
             password='1321ased',
             role=RolesEnum.USER,
+            is_active=True,
+            otp="111222",
+            image="https://picsum.photos/200/300"
+        )
+
+        mock_repo.return_value.update.return_value = UserDB(
+            username="test@mail.com",
+            password='1321ased',
+            role=RolesEnum.USER,
+            is_active=True,
+            otp="111222",
+            image="https://picsum.photos/200/300"
+        )
+
+        user_service = UserServices(mock_db)
+        result = user_service.activate_user(activation_data)
+        assert result.is_active is True
+        mock_repo.return_value.update.assert_called()
+
+
+def test_activation_not_success(activation_data, mock_db):
+    with patch("app.services.users.UserRepo") as mock_repo:
+        mock_repo.return_value.get_by_username.return_value = UserDB(
+            username="test@mail.com",
+            password='1321ased',
+            role=RolesEnum.USER,
             is_active=False,
             otp="123456",
             image="https://picsum.photos/200/300"
         )
+
+        mock_repo.return_value.update.return_value = UserDB(
+            username="test@mail.com",
+            password='1321ased',
+            role=RolesEnum.USER,
+            is_active=False,
+            otp="123456",
+            image="https://picsum.photos/200/300"
+        )
+
         user_service = UserServices(mock_db)
         result = user_service.activate_user(activation_data)
-        print("///////////////////////////")
-        print(result)
+        assert result.is_active is False
+        mock_repo.return_value.update.assert_not_called()
